c3d1eddb10a7a8b5c5b9c91868a7116c8baed9a6
Alexey <yakor.alex@gmail.com>
2023-03-07 16:22:50 +0100

    update gendiff/scripts/gendiff.py, fix pyproject.toml

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,13 +4,14 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
     args = parser.parse_args()
-    print(f'{args.first_file}{args.second_file}')
 
+    print(f'First file: {args.first_file}')
+    print(f'Second file: {args.second_file}')
 
 if __name__ == '__main__':
     main()
