bf98f8f079856de94fec9d3b067878da833d789d
Alexey <yakor.alex@gmail.com>
2023-03-08 08:47:57 +0100

    update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+    parser.add_argument('-f', '--format', help="set format of output")
     args = parser.parse_args()
 
     print(f'First file: {args.first_file}')
