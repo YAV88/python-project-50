17e660404eb035ae192b6216ea945094323abb1d
Alexey <yakor.alex@gmail.com>
2023-03-09 10:10:58 +0100

    update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -29,6 +29,8 @@ def main():
     )
     parser.add_argument('first_file', help='path to the first file')
     parser.add_argument('second_file', help='path to the second file')
+    parser.add_argument('-f', '--format', help='set format of output',
+                        default='stylish')
     args = parser.parse_args()
 
     diff = generate_diff(args.first_file, args.second_file)
