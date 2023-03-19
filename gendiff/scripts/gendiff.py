2fb9c40d1fe88480fca2ba5beaf5557935c6c64c
Alexey <yakor.alex@gmail.com>
2023-03-19 21:11:03 +0100

    update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,39 +2,9 @@
 
 import argparse
 
+from gendiff.compare_data import compare_data
+from gendiff.format_diff import format_diff
 from gendiff.parsing import load_data
-from collections import defaultdict
-
-
-def compare_data(data1, data2):
-    diff = defaultdict(dict)
-
-    for key in data1.keys() | data2.keys():
-        if key in data1 and key in data2:
-            if data1[key] == data2[key]:
-                diff[key][" "] = data1[key]
-            else:
-                diff[key]["-"] = data1[key]
-                diff[key]["+"] = data2[key]
-        elif key in data1:
-            diff[key]["-"] = data1[key]
-        else:
-            diff[key]["+"] = data2[key]
-
-    return diff
-
-
-def format_diff(diff):
-    diff_lines = []
-    for key, values in sorted(diff.items()):
-        for symbol, value in values.items():
-            if isinstance(value, bool):
-                value = str(value).lower()
-            if isinstance(value, str):
-                value = f'{value}'
-            diff_lines.append(f'  {symbol} {key}: {value}')
-
-    return '{\n' + '\n'.join(diff_lines) + '\n}'
 
 
 def generate_diff(file_path1, file_path2):
