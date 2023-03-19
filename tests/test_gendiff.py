2befb39d5b9b5037af7be28b5dab9e93aace5adf
Alexey <yakor.alex@gmail.com>
2023-03-19 21:14:09 +0100

    update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -22,3 +22,14 @@ def test_generate_diff_yml(expected_result):
     file_path2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file2.yml')
     result = generate_diff(file_path1, file_path2)
     assert result == expected_result
+
+
+def test_nested_structures_json():
+    file_path1 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file3.json')
+    file_path2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file4.json')
+    expected_result = os.path.join(os.path.dirname(__file__), 'fixtures', 'nested_structures_result_updated.txt')
+
+    with open(expected_result) as f:
+        expected = f.read()
+
+    assert generate_diff(file_path1, file_path2) == expected
