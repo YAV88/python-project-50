a04c5f86704da7a7feac9b0ece383df5bdc1e4e3
Alexey <yakor.alex@gmail.com>
2023-03-24 17:50:24 +0100

    update gendiff/compare_data.py

--- gendiff/compare_data.py
+++ gendiff/compare_data.py
@@ -1,19 +1,15 @@
-from collections import defaultdict
-
-
 def compare_data(data1, data2):
-    diff = defaultdict(dict)
+    diff = {}
 
     for key in data1.keys() | data2.keys():
         if key in data1 and key in data2:
             if data1[key] == data2[key]:
-                diff[key][" "] = data1[key]
+                diff[key] = {" ": data1[key]}
             else:
-                diff[key]["-"] = data1[key]
-                diff[key]["+"] = data2[key]
+                diff[key] = {"-": data1[key], "+": data2[key]}
         elif key in data1:
-            diff[key]["-"] = data1[key]
+            diff[key] = {"-": data1[key]}
         else:
-            diff[key]["+"] = data2[key]
+            diff[key] = {"+": data2[key]}
 
     return diff
