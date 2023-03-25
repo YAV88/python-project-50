7d56a7234192e88b84f418cf3b01ca1caca6b9ba
Alexey <yakor.alex@gmail.com>
2023-03-25 02:21:38 +0100

    fix gendiff/compare_data.py

--- gendiff/compare_data.py
+++ gendiff/compare_data.py
@@ -3,15 +3,15 @@ def compare_data(obj1, obj2):
     all_keys = set(obj1.keys()) | set(obj2.keys())
     for key in sorted(all_keys):
         if key in obj1 and key not in obj2:
-            diff['-' + key] = obj1[key]
+            diff['-' + ' ' + key] = obj1[key]
         elif key in obj2 and key not in obj1:
-            diff['+' + key] = obj2[key]
+            diff['+' + ' ' + key] = obj2[key]
         elif obj1[key] != obj2[key]:
             if isinstance(obj1[key], dict) and isinstance(obj2[key], dict):
                 diff[key] = compare_data(obj1[key], obj2[key])
             else:
-                diff['-' + key] = obj1[key]
-                diff['+' + key] = obj2[key]
+                diff['-' + ' ' + key] = obj1[key]
+                diff['+' + ' ' + key] = obj2[key]
         else:
-            diff[key] = obj1[key]
+            diff[' ' + ' ' + key] = obj1[key]
     return diff
