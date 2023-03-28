3551e32dacb7856fc0385749ebc83065970829f0
Alexey <yakor.alex@gmail.com>
2023-03-28 10:53:09 +0200

    update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,7 @@
 import argparse
 
 from gendiff.compare_data import compare_data
-from gendiff.format_diff import format_diff
+from gendiff.formatters.stylish import format_diff
 from gendiff.parser import load_data
 
 
@@ -11,7 +11,10 @@ def generate_diff(file_path1, file_path2, format_='stylish'):
     data1 = load_data(file_path1)
     data2 = load_data(file_path2)
     diff = compare_data(data1, data2)
-    return format_diff(diff)
+    if format_ == 'stylish':
+        return format_diff(diff)
+    else:
+        raise ValueError(f"Unknown output format: {format_}")
 
 
 def main():
@@ -24,6 +27,9 @@ def main():
     parser.add_argument('-f', '--format', help='set format of output',
                         default='stylish')
     args = parser.parse_args()
+
+    if args.format not in ['stylish', 'plain', 'json']:
+        raise ValueError(f"Unknown output format: {args.format}")
 
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
