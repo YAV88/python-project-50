d41ec4e5a4d9823c6dd43397ec7e84169a7d4760
Alexey <yakor.alex@gmail.com>
2023-04-08 16:59:50 +0200

    fix gendiff/compare_data.py

--- gendiff/compare_data.py
+++ gendiff/compare_data.py
@@ -1,22 +1,40 @@
-def compare_data(obj1, obj2):
-    diff = {}
+def value_to_string(value):
+    if isinstance(value, dict):
+        return '[complex value]'
+    elif isinstance(value, str):
+        return f"'{value}'"
+    elif value is None:
+        return 'null'
+    elif isinstance(value, bool):
+        return str(value).lower()
+    else:
+        return str(value)
+
+
+def compare_data(obj1, obj2, path=''):
+    diff = []
     all_keys = set(obj1.keys()) | set(obj2.keys())
     for key in sorted(all_keys):
+        new_path = f'{path}.{key}' if path else key
         if key in obj1 and key not in obj2:
-            diff['-' + ' ' + key] = obj1[key]
+            diff.append(f"Property '{new_path}' was removed")
         elif key in obj2 and key not in obj1:
-            diff['+' + ' ' + key] = obj2[key]
-        elif obj1.get(key) != obj2.get(key):
-            if isinstance(obj1.get(key), dict) and \
-                    isinstance(obj2.get(key), dict):
-                diff[key] = compare_data(obj1[key], obj2[key])
-            elif obj1.get(key) is None and obj2.get(key) is False:
-                diff[' ' + ' ' + key] = obj1.get(key)
-            elif obj1.get(key) is False and obj2.get(key) is None:
-                diff[' ' + ' ' + key] = obj2.get(key)
+            if isinstance(obj2[key], dict):
+                diff += compare_data({}, obj2[key], new_path)
             else:
-                diff['-' + ' ' + key] = obj1.get(key)
-                diff['+' + ' ' + key] = obj2.get(key)
+                diff.append(f"Property '{new_path}' was added with value: {value_to_string(obj2[key])}")
+        elif obj1.get(key) != obj2.get(key):
+            if isinstance(obj1.get(key), dict) and isinstance(obj2.get(key), dict):
+                diff += compare_data(obj1[key], obj2[key], new_path)
+            elif obj1.get(key) is None and obj2.get(key) is False:
+                diff.append(f"Property '{new_path}' was updated. From {obj1.get(key)} to {obj2.get(key)}")
+            elif obj1.get(key) is False and obj2.get(key) is None:
+                diff.append(f"Property '{new_path}' was updated. From {obj1.get(key)} to {obj2.get(key)}")
+            else:
+                diff.append(f"Property '{new_path}' was updated. From {value_to_string(obj1.get(key))} to {value_to_string(obj2.get(key))}")
         else:
-            diff[' ' + ' ' + key] = obj1.get(key)
+            if isinstance(obj1.get(key), dict) and isinstance(obj2.get(key), dict):
+                diff += compare_data(obj1[key], obj2[key], new_path)
+            else:
+                diff.append(f"Property '{new_path}' was not changed")
     return diff
