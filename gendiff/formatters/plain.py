88277cc451c6be7e1ccdd7e49d4b40d795846d04
Alexey <yakor.alex@gmail.com>
2023-04-08 17:00:20 +0200

    fix gendiff/formatters/plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,16 +1,6 @@
-def format_diff_plain(diff, parent_key=''):
-    lines = []
-    for key, value in diff.items():
-        if isinstance(value, dict):
-            nested_parent_key = f"{parent_key}.{key}" if parent_key else key
-            lines.append(format_diff_plain(value, nested_parent_key))
-        else:
-            if value is None:
-                value = 'null'
-            elif value is True:
-                value = 'true'
-            elif value is False:
-                value = 'false'
-            nested_key = f"{parent_key}.{key}" if parent_key else key
-            lines.append(f"Property '{nested_key}' was {value}")
-    return '\n'.join(lines)
+def format_diff_plain(diff):
+    result = []
+    for item in diff:
+        result.append(item)
+    return '\n'.join(result)
+
