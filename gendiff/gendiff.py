ed6262092bcc539d039d3440164228416cb23369
Alexey <yakor.alex@gmail.com>
2023-04-15 16:59:28 +0200

    fix gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,17 +1,17 @@
-from gendiff.compare_data import compare_data
-from gendiff.formatters.stylish import format_diff_stylish
+from gendiff.get_diff import get_diff
 from gendiff.parser import load_data
-from gendiff.formatters.plain import format_diff_plain
-from gendiff.formatters.json import format_diff_json
+from gendiff.formatters.stylish import format_stylish
+from gendiff.formatters.plain import format_plain
+from gendiff.formatters.json import format_json
 
 
 def generate_diff(file_path1, file_path2, format_='stylish'):
     data1 = load_data(file_path1)
     data2 = load_data(file_path2)
-    diff = compare_data(data1, data2)
+    diff = get_diff(data1, data2)
     if format_ == 'plain':
-        return format_diff_plain(diff)
+        return format_plain(diff)
     elif format_ == 'json':
-        return format_diff_json(diff)
+        return format_json(diff)
     else:
-        return format_diff_stylish(diff)
+        return format_stylish(diff)
