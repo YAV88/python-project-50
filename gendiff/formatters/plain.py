f5d9819ad72e946b465738063b790a48a71d16a6
Alexey <yakor.alex@gmail.com>
2023-04-18 16:16:04 +0200

    update gendiff/formatters/plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -9,29 +9,39 @@ def format_plain(diff, parent=''):
     for key in sorted(diff.keys()):  # Сортировка ключей
         value = diff[key]
         key_path = f"{parent}.{key}" if parent else key
+
         if isinstance(value, dict):
             if value.get('status') == 'added':
                 if isinstance(value.get('new_value'), dict):
-                    lines.append(f"Property '{key_path}' was added with value: [complex value]")
+                    lines.append(f"Property '{key_path}' "
+                                 f"was added with value: [complex value]")
                 else:
-                    lines.append(f"Property '{key_path}' was added with value: {format_value(value['new_value'])}")
+                    lines.append(f"Property '{key_path}' "
+                                 f"was added with value: "
+                                 f"{format_value(value['new_value'])}")
+
             elif value.get('status') == 'removed':
-                lines.append(f"Property '{key_path}' was removed")
+                lines.append(f"Property '{key_path}' "
+                             f"was removed")
+
             elif value.get('status') == 'updated':
                 old_value = format_value(value['old_value'])
                 new_value = format_value(value['new_value'])
-                lines.append(f"Property '{key_path}' was updated. From {old_value} to {new_value}")
+                lines.append(f"Property '{key_path}' "
+                             f"was updated. From {old_value} to {new_value}")
+
             elif isinstance(value, dict):
                 nested_lines = format_plain(value, parent=key_path)
                 lines.append(nested_lines)
+
     return '\n'.join(lines)
 
 
 def format_value(value):
     """
-    Форматирует значение для отображения в plain тексте
-    :param value: Значение
-    :return: Отформатированное значение
+    Formats the value to be displayed in plain text
+    :param value: Value
+    :return: Formatted value
     """
     if isinstance(value, str):
         return f"'{value}'"
