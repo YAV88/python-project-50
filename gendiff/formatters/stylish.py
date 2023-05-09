0478c0d9a10904ec1dc9aafbd8b569d193693cf4
Alexey <yakor.alex@gmail.com>
2023-05-09 17:17:57 +0200

    update gendiff/formatters/stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,19 +1,30 @@
-import json
+def format_stylish(diff, depth=0):
+    result = []
+    for key, node in sorted(diff.items()):
+        status = node.get("status")
+        old_value = node.get("old_value")
+        new_value = node.get("new_value")
+        indent = "  " * depth
+        if status == "nested":
+            result.append(f"  {indent}  {key}: {format_stylish(node['children'], depth+2)}")
+        elif status == "added":
+            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}")
+        elif status == "removed":
+            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}")
+        elif status == "updated":
+            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}")
+            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}")
+        else:  # status == "unchanged"
+            result.append(f"  {indent}  {key}: {format_value(old_value, depth+2)}")
+    return "{\n" + "\n".join(result) + "\n" + "  " * depth + "}"
 
 
-def format_diff_stylish(diff, depth=0, indent_size=4):
-    indent = ' ' * depth * indent_size
-    lines = []
-    for key, value in diff.items():
-        if isinstance(value, dict):
-            lines.append(f"{indent}  {key}: {{")
-            lines.append(format_diff_stylish(value, depth+1, indent_size))
-            lines.append(f"{indent}  }}")
-        elif isinstance(value, str):
-            lines.append(f"{indent}  {key}: {value}")
-        else:
-            lines.append(f"{indent}  {key}: {json.dumps(value)}")
-    if depth == 0:
-        return '{\n' + '\n'.join(lines) + '\n}'
-    else:
-        return '\n'.join(lines)
+def format_value(value, depth):
+    if isinstance(value, dict):
+        items = []
+        for key, node in sorted(value.items()):
+            items.append(f"{depth * '  '}    {key}: {format_value(node, depth + 2)}")
+        return "{\n" + "\n".join(items) + "\n" + depth * '  ' + "}"
+    elif isinstance(value, bool):
+        return str(value).lower()
+    return str(value)
