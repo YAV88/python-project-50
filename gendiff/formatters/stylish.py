53681b9655f3543f75130c940168bb8fa3b8e8ef
Alexey <yakor.alex@gmail.com>
2023-05-10 10:41:48 +0200

    fix gendiff/formatters/stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -6,16 +6,16 @@ def format_stylish(diff, depth=0):
         new_value = node.get("new_value")
         indent = "  " * depth
         if status == "nested":
-            result.append(f"  {indent}  {key}: {format_stylish(node['children'], depth+2)}")
+            result.append(f"  {indent}  {key}: {format_stylish(node['children'], depth+2)}".rstrip())
         elif status == "added":
-            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}")
+            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}".rstrip())
         elif status == "removed":
-            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}")
+            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}".rstrip())
         elif status == "updated":
-            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}")
-            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}")
+            result.append(f"  {indent}- {key}: {format_value(old_value, depth+2)}".rstrip())
+            result.append(f"  {indent}+ {key}: {format_value(new_value, depth+2)}".rstrip())
         else:  # status == "unchanged"
-            result.append(f"  {indent}  {key}: {format_value(old_value, depth+2)}")
+            result.append(f"  {indent}  {key}: {format_value(old_value, depth+2)}".rstrip())
     return "{\n" + "\n".join(result) + "\n" + "  " * depth + "}"
 
 
@@ -23,7 +23,7 @@ def format_value(value, depth):
     if isinstance(value, dict):
         items = []
         for key, node in sorted(value.items()):
-            items.append(f"{depth * '  '}    {key}: {format_value(node, depth + 2)}")
+            items.append(f"{depth * '  '}    {key}: {format_value(node, depth + 2)}".rstrip())
         return "{\n" + "\n".join(items) + "\n" + depth * '  ' + "}"
     elif isinstance(value, bool):
         return str(value).lower()
