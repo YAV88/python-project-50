644149776feb3d50ba4a2b99a2a070a7d92e9633
Alexey <yakor.alex@gmail.com>
2023-05-12 19:51:35 +0200

    update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,6 +2,8 @@ import pytest
 import json
 from gendiff.formatters.stylish import format_stylish
 from gendiff.get_diff import get_diff
+from gendiff.formatters.json import format_json
+from gendiff.formatters.plain import format_plain
 
 
 @pytest.fixture()
@@ -40,6 +42,12 @@ def nested_structures_result():
         return f.read()
 
 
+@pytest.fixture()
+def expected_result_json():
+    with open("tests/fixtures/expected_result_json.txt") as f:
+        return f.read()
+
+
 def test_get_diff(file1, file2, expected_result):
     diff = get_diff(file1, file2)
     assert format_stylish(diff) == expected_result
@@ -48,3 +56,16 @@ def test_get_diff(file1, file2, expected_result):
 def test_format_stylish(file3, file4, nested_structures_result):
     diff = get_diff(file3, file4)
     assert format_stylish(diff) == nested_structures_result
+
+
+def test_format_json(file1, file2, expected_result_json):
+    diff = get_diff(file1, file2)
+    assert format_json(diff) == expected_result_json
+
+
+def test_format_plain(file1, file2):
+    diff = get_diff(file1, file2)
+    assert format_plain(diff) == """Property 'follow' was removed
+Property 'proxy' was removed
+Property 'timeout' was updated. From [complex value] to [complex value]
+Property 'verbose' was added with value: true"""
