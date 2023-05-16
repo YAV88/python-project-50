375602094cb976c5d2e51d60108418cf015acca0
Alexey <yakor.alex@gmail.com>
2023-05-16 10:33:27 +0200

    fix tests/fixtures/nested_structures_result.txt and gendiff/formatters/stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -11,27 +11,29 @@ def format_stylish(diff, depth=0):
                           .rstrip())
         elif status == "added":
             result.append(f"  {indent}+ {key}: "
-                          f"{format_value(new_value, depth+2)}".rstrip())
+                          f"{format_value(new_value, depth+2)}")
         elif status == "removed":
             result.append(f"  {indent}- {key}: "
-                          f"{format_value(old_value, depth+2)}".rstrip())
+                          f"{format_value(old_value, depth+2)}")
         elif status == "updated":
             result.append(f"  {indent}- {key}: "
-                          f"{format_value(old_value, depth+2)}".rstrip())
+                          f"{format_value(old_value, depth+2)}")
             result.append(f"  {indent}+ {key}: "
-                          f"{format_value(new_value, depth+2)}".rstrip())
-        else:  # status == "unchanged"
+                          f"{format_value(new_value, depth+2)}")
+        else:
             result.append(f"  {indent}  {key}: "
-                          f"{format_value(old_value, depth+2)}".rstrip())
+                          f"{format_value(old_value, depth+2)}")
     return "{\n" + "\n".join(result) + "\n" + "  " * depth + "}"
 
 
 def format_value(value, depth):
-    if isinstance(value, dict):
+    if value is None:
+        return "null"
+    elif isinstance(value, dict):
         items = []
         for key, node in sorted(value.items()):
             items.append(f"{depth * '  '}    {key}: "
-                         f"{format_value(node, depth + 2)}".rstrip())
+                         f"{format_value(node, depth + 2)}")
         return "{\n" + "\n".join(items) + "\n" + depth * '  ' + "}"
     elif isinstance(value, bool):
         return str(value).lower()
