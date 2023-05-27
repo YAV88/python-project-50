bdd6ee82ee97cb803cf0d9b3e8a6f9f795172b6b
Alexey <yakor.alex@gmail.com>
2023-05-27 23:41:06 +0200

    transferring a function from gendiff/scripts/gendiff.py in gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import os
+import argparse
 
 
 def load_data(file_path):
@@ -12,3 +13,16 @@ def load_data(file_path):
             return yaml.safe_load(file)
         else:
             raise ValueError(f"Unknown file extension: {extension}")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', help='path to the first file')
+    parser.add_argument('second_file', help='path to the second file')
+    parser.add_argument('-f', '--format', help='set format of output',
+                        choices=['stylish', 'plain', 'json'],
+                        default='stylish')
+    return parser.parse_args()
