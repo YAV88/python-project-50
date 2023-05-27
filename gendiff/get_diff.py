baaff78364ad2ff4967c93122e3255ea49ee8efc
Alexey <yakor.alex@gmail.com>
2023-05-28 00:41:42 +0200

    shortened the lines for the linter in gendiff/get_diff.py and gendiff/formatters/plain.py

--- gendiff/get_diff.py
+++ gendiff/get_diff.py
@@ -13,18 +13,27 @@ def get_diff(obj1, obj2, key=''):
         key_path = f"{key}.{sorted_key}" if key else sorted_key
 
         if sorted_key not in obj1:
-            diff[sorted_key] = {"status": "added", "new_value": obj2[sorted_key]}
+            diff[sorted_key] = {"status": "added",
+                                "new_value": obj2[sorted_key]}
         elif sorted_key not in obj2:
-            diff[sorted_key] = {"status": "removed", "old_value": obj1[sorted_key]}
+            diff[sorted_key] = {"status": "removed",
+                                "old_value": obj1[sorted_key]}
         elif obj1[sorted_key] == obj2[sorted_key]:
-            diff[sorted_key] = {"status": "unchanged", "old_value": obj1[sorted_key]}
-        elif isinstance(obj1[sorted_key], dict) and isinstance(obj2[sorted_key], dict):
-            inner_diff = get_diff(obj1[sorted_key], obj2[sorted_key], key_path)
+            diff[sorted_key] = {"status": "unchanged",
+                                "old_value": obj1[sorted_key]}
+        elif isinstance(obj1[sorted_key], dict) and \
+                isinstance(obj2[sorted_key], dict):
+            inner_diff = get_diff(obj1[sorted_key],
+                                  obj2[sorted_key], key_path)
             if inner_diff:
-                diff[sorted_key] = {"status": "nested", "children": inner_diff}
+                diff[sorted_key] = {"status": "nested",
+                                    "children": inner_diff}
         else:
             old_value = obj1[sorted_key]
-            new_value = obj2[sorted_key] if obj2[sorted_key] is not None else None
-            diff[sorted_key] = {"status": "updated", "old_value": old_value, "new_value": new_value}
+            new_value = obj2[sorted_key] \
+                if obj2[sorted_key] is not None else None
+            diff[sorted_key] = {"status": "updated",
+                                "old_value": old_value,
+                                "new_value": new_value}
 
     return diff
