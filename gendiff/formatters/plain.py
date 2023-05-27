5099373f2822b27bbce960dfef47ea5d47987298
Alexey <yakor.alex@gmail.com>
2023-05-28 00:33:46 +0200

    fix gendiff/formatters/plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -4,31 +4,27 @@ def format_plain(diff, parent=''):
         value = diff[key]
         key_path = f"{parent}.{key}" if parent else key
 
-        if isinstance(value, dict):
-            if value.get('status') == 'added':
-                if isinstance(value.get('new_value'), dict):
-                    line = f"Property '{key_path}' " \
-                           f"was added with value: [complex value]"
-                elif value['new_value'] is None:
-                    line = f"Property '{key_path}' was added with value: null"
-                else:
-                    line = f"Property '{key_path}' was added with value: " \
-                           f"{format_value(value['new_value'])}"
-                lines.append(line)
+        if isinstance(value, dict) and value.get('status') == 'added':
+            if isinstance(value.get('new_value'), dict):
+                line = f"Property '{key_path}' was added with value: [complex value]"
+            elif value['new_value'] is None:
+                line = f"Property '{key_path}' was added with value: null"
+            else:
+                line = f"Property '{key_path}' was added with value: {format_value(value['new_value'])}"
+            lines.append(line)
 
-            elif value.get('status') == 'removed':
-                lines.append(f"Property '{key_path}' was removed")
+        elif isinstance(value, dict) and value.get('status') == 'removed':
+            lines.append(f"Property '{key_path}' was removed")
 
-            elif value.get('status') == 'updated':
-                old_value = format_value(value['old_value'])
-                new_value = format_value(value['new_value'])
-                line = f"Property '{key_path}' was updated. " \
-                       f"From {old_value} to {new_value}"
-                lines.append(line)
+        elif isinstance(value, dict) and value.get('status') == 'updated':
+            old_value = format_value(value['old_value'])
+            new_value = format_value(value['new_value'])
+            line = f"Property '{key_path}' was updated. From {old_value} to {new_value}"
+            lines.append(line)
 
-            elif value.get('status') == 'nested':
-                nested_lines = format_plain(value['children'], parent=key_path)
-                lines.append(nested_lines)
+        elif isinstance(value, dict) and value.get('status') == 'nested':
+            nested_lines = format_plain(value['children'], parent=key_path)
+            lines.append(nested_lines)
 
     return '\n'.join(lines)
 
