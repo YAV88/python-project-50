1fe19bbe802922a1df7b72f1819d13b79585fd37
Alexey <yakor.alex@gmail.com>
2023-05-28 00:02:05 +0200

    fix gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,15 +4,24 @@ import os
 import argparse
 
 
-def load_data(file_path):
+def read_file(file_path):
     with open(file_path) as file:
-        _, extension = os.path.splitext(file_path)
-        if extension == '.json':
-            return json.load(file)
-        elif extension in ['.yaml', '.yml']:
-            return yaml.safe_load(file)
-        else:
-            raise ValueError(f"Unknown file extension: {extension}")
+        return file.read()
+
+
+def parse_data(data, file_extension):
+    if file_extension == '.json':
+        return json.loads(data)
+    elif file_extension in ['.yaml', '.yml']:
+        return yaml.safe_load(data)
+    else:
+        raise ValueError(f"Unknown file extension: {file_extension}")
+
+
+def load_data(file_path):
+    _, extension = os.path.splitext(file_path)
+    file_data = read_file(file_path)
+    return parse_data(file_data, extension)
 
 
 def parse_args():
