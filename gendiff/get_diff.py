fb1488afa1e1ecf69fe3d9baaf2a3da6a8137b37
Alexey <yakor.alex@gmail.com>
2023-05-28 00:28:39 +0200

    fix gendiff/get_diff.py

--- gendiff/get_diff.py
+++ gendiff/get_diff.py
@@ -9,55 +9,22 @@ def get_diff(obj1, obj2, key=''):
     diff = {}
     keys = set(obj1.keys()) | set(obj2.keys())
 
-    for k in sorted(keys):
-        key_path = f"{key}.{k}" if key else k
+    for sorted_key in sorted(keys):
+        key_path = f"{key}.{sorted_key}" if key else sorted_key
 
-        if k not in obj1:
-            diff[k] = added_diff(obj2[k])
-        elif k not in obj2:
-            diff[k] = removed_diff(obj1[k])
+        if sorted_key not in obj1:
+            diff[sorted_key] = {"status": "added", "new_value": obj2[sorted_key]}
+        elif sorted_key not in obj2:
+            diff[sorted_key] = {"status": "removed", "old_value": obj1[sorted_key]}
+        elif obj1[sorted_key] == obj2[sorted_key]:
+            diff[sorted_key] = {"status": "unchanged", "old_value": obj1[sorted_key]}
+        elif isinstance(obj1[sorted_key], dict) and isinstance(obj2[sorted_key], dict):
+            inner_diff = get_diff(obj1[sorted_key], obj2[sorted_key], key_path)
+            if inner_diff:
+                diff[sorted_key] = {"status": "nested", "children": inner_diff}
         else:
-            if obj1[k] == obj2[k]:
-                diff[k] = {"status": "unchanged", "old_value": obj1[k]}
-            elif isinstance(obj1[k], dict) and isinstance(obj2[k], dict):
-                inner_diff = get_diff(obj1[k], obj2[k], key_path)
-                if inner_diff:
-                    diff[k] = {"status": "nested", "children": inner_diff}
-            else:
-                old_value = obj1[k]
-                new_value = obj2[k] if obj2[k] is not None else None
-                diff[k] = updated_diff(old_value, new_value)
+            old_value = obj1[sorted_key]
+            new_value = obj2[sorted_key] if obj2[sorted_key] is not None else None
+            diff[sorted_key] = {"status": "updated", "old_value": old_value, "new_value": new_value}
 
-    return diff if diff else {}
-
-
-def added_diff(new_value):
-    """
-    Creates a diff for an added value
-    :param new_value: New value that was added
-    :return: Difference object for added value
-    """
-    return {"status": "added",
-            "new_value": new_value}
-
-
-def removed_diff(old_value):
-    """
-    Creates a diff for a removed value
-    :param old_value: Old value that was removed
-    :return: Difference object for removed value
-    """
-    return {"status": "removed",
-            "old_value": old_value}
-
-
-def updated_diff(old_value, new_value):
-    """
-    Creates a diff for an updated value
-    :param old_value: Old value before update
-    :param new_value: New value after update
-    :return: Difference object for updated value
-    """
-    return {"status": "updated",
-            "old_value": old_value,
-            "new_value": new_value if new_value is not None else None}
+    return diff
