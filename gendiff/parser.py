052099e92f8049518eb6712109c53f7b8b77d85e
Alexey <yakor.alex@gmail.com>
2023-05-29 13:09:12 +0200

    fix gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,7 +1,6 @@
 import json
 import yaml
 import os
-import argparse
 
 
 def read_file(file_path):
@@ -22,16 +21,3 @@ def load_data(file_path):
     _, extension = os.path.splitext(file_path)
     file_data = read_file(file_path)
     return parse_data(file_data, extension)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('first_file', help='path to the first file')
-    parser.add_argument('second_file', help='path to the second file')
-    parser.add_argument('-f', '--format', help='set format of output',
-                        choices=['stylish', 'plain', 'json'],
-                        default='stylish')
-    return parser.parse_args()
