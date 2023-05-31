0f22a7eee929418a8081ede680b83f995846a9d2
Alexey <yakor.alex@gmail.com>
2023-05-31 18:40:54 +0200

    fix gendiff/get_diff.py

--- gendiff/get_diff.py
+++ gendiff/get_diff.py
@@ -9,31 +9,30 @@ def get_diff(obj1, obj2, key=''):
     diff = {}
     keys = set(obj1.keys()) | set(obj2.keys())
 
-    for sorted_key in sorted(keys):
-        key_path = f"{key}.{sorted_key}" if key else sorted_key
+    for keyword in sorted(keys):
+        key_path = f"{key}.{keyword}" if key else keyword
 
-        if sorted_key not in obj1:
-            diff[sorted_key] = {"status": "added",
-                                "new_value": obj2[sorted_key]}
-        elif sorted_key not in obj2:
-            diff[sorted_key] = {"status": "removed",
-                                "old_value": obj1[sorted_key]}
-        elif obj1[sorted_key] == obj2[sorted_key]:
-            diff[sorted_key] = {"status": "unchanged",
-                                "old_value": obj1[sorted_key]}
-        elif isinstance(obj1[sorted_key], dict) and \
-                isinstance(obj2[sorted_key], dict):
-            inner_diff = get_diff(obj1[sorted_key],
-                                  obj2[sorted_key], key_path)
+        if keyword not in obj1:
+            diff[keyword] = {"status": "added", "new_value": obj2[keyword]}
+        elif keyword not in obj2:
+            diff[keyword] = {"status": "removed",
+                             "old_value": obj1[keyword]}
+        elif obj1[keyword] == obj2[keyword]:
+            diff[keyword] = {"status": "unchanged",
+                             "old_value": obj1[keyword]}
+        elif isinstance(obj1[keyword], dict) and \
+                isinstance(obj2[keyword], dict):
+            inner_diff = get_diff(obj1[keyword],
+                                  obj2[keyword], key_path)
             if inner_diff:
-                diff[sorted_key] = {"status": "nested",
-                                    "children": inner_diff}
+                diff[keyword] = {"status": "nested",
+                                 "children": inner_diff}
         else:
-            old_value = obj1[sorted_key]
-            new_value = obj2[sorted_key] \
-                if obj2[sorted_key] is not None else None
-            diff[sorted_key] = {"status": "updated",
-                                "old_value": old_value,
-                                "new_value": new_value}
+            old_value = obj1[keyword]
+            new_value = obj2[keyword] \
+                if obj2[keyword] is not None else None
+            diff[keyword] = {"status": "updated",
+                             "old_value": old_value,
+                             "new_value": new_value}
 
     return diff
