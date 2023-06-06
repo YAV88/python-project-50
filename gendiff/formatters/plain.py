008e5774ab2622f4efbf836fdbafbc271b58c77c
Alexey <yakor.alex@gmail.com>
2023-06-06 09:16:45 +0200

    fix gendiff/formatters/plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -5,22 +5,22 @@ def format_plain(diff, parent=''):
         key_path = f"{parent}.{key}" if parent else key
 
         if isinstance(value, dict):
-            if value.get('status') == 'added':
+            if value.get('type') == 'added':
                 line = f"Property '{key_path}' was added with value: " \
                        f"{format_value(value.get('new_value'))}"
                 lines.append(line)
 
-            elif value.get('status') == 'removed':
+            elif value.get('type') == 'removed':
                 lines.append(f"Property '{key_path}' was removed")
 
-            elif value.get('status') == 'updated':
+            elif value.get('type') == 'updated':
                 old_value = format_value(value['old_value'])
                 new_value = format_value(value['new_value'])
                 line = f"Property '{key_path}' was updated. " \
                        f"From {old_value} to {new_value}"
                 lines.append(line)
 
-            elif value.get('status') == 'nested':
+            elif value.get('type') == 'nested':
                 nested_lines = format_plain(value['children'], parent=key_path)
                 lines.append(nested_lines)
 
